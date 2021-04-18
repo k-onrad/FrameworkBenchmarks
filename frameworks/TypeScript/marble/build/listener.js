71208f415e4529c7fa6efbc614726f29770d50fe
Gustavo Konrad <g.konrad@outlook.com>
2021-04-18 12:42:23 -0300

    flamegraph

--- frameworks/TypeScript/marble/build/listener.js
+++ frameworks/TypeScript/marble/build/listener.js
@@ -5,11 +5,14 @@ const core_1 = require("@marblejs/core");
 // import { logger$ } from '@marblejs/middleware-logger'
 const middleware_body_1 = require("@marblejs/middleware-body");
 const api_1 = require("./api");
+const middlewares = [
+    middleware_body_1.bodyParser$(),
+];
 const effects = [
     api_1.json$,
     api_1.plaintext$,
 ];
 exports.listener = core_1.httpListener({
-    middlewares: [middleware_body_1.bodyParser$],
+    middlewares,
     effects,
 });
